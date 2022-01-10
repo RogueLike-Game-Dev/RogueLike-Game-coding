fe37064f0ba3e2eb7b4818ae93bd38f81a5e58d0
Paula Iuga <paula.iuga@s.unibuc.ro>
2022-01-10 13:25:21 +0200

    added volume settings + credits/keybindings/save slot board

--- Assets/Scripts/ButtonsFuntions.cs
+++ Assets/Scripts/ButtonsFuntions.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 public class ButtonsFuntions : MonoBehaviour
 {
     private GameObject smallBoard;
-    
+    private GameObject settingsBoard;
+    private GameObject volumeBoard;
+    private GameObject keybindingsBoard;
+    private GameObject creditsBoard;
+    private GameObject saveSlot;
+
     void Start()
     {
         smallBoard = GameObject.Find("SmallBoard");
+        settingsBoard = GameObject.Find("SettingsBoard");
+        volumeBoard = GameObject.Find("VolumeBoard");
+        keybindingsBoard = GameObject.Find("KeyBindings");
+        creditsBoard = GameObject.Find("CreditsBoard");
+        saveSlot = GameObject.Find("SaveSlotBoard");
+        
         if (smallBoard)
         {
             smallBoard.SetActive(false);
+        }
+        
+        if (settingsBoard != null && volumeBoard != null && keybindingsBoard != null 
+            && creditsBoard != null && saveSlot != null)
+        {
+            settingsBoard.SetActive(false);
+            volumeBoard.SetActive(false);
+            keybindingsBoard.SetActive(false);
+            creditsBoard.SetActive(false);
+            saveSlot.SetActive(false);
         }
     }
 
@@ -33,6 +54,31 @@ public class ButtonsFuntions : MonoBehaviour
         smallBoard.SetActive(true);
     }
 
+    public void OpenSettingsBoard()
+    {
+        settingsBoard.SetActive(true);
+    }
+    
+    public void OpenSaveSlotBoard()
+    {
+        saveSlot.SetActive(true);
+    }
+
+    public void OpenVolumeBoard()
+    {
+        volumeBoard.SetActive(true);
+    }
+
+    public void OpenCreditsBoard()
+    {
+        creditsBoard.SetActive(true);
+    }
+    
+    public void OpenKeybindingsBoard()
+    {
+        keybindingsBoard.SetActive(true);
+    }
+
     public void GameQuit()
     {
         EditorApplication.isPlaying = false;
@@ -42,5 +88,30 @@ public class ButtonsFuntions : MonoBehaviour
     public void CloseSmallBanner()
     {
         smallBoard.SetActive(false);
+    }
+    
+    public void CloseCreditsBanner()
+    {
+        creditsBoard.SetActive(false);
+    }
+    
+    public void CloseSettingsBanner()
+    {
+        settingsBoard.SetActive(false);
+    }
+    
+    public void CloseSaveSlotBanner()
+    {
+        saveSlot.SetActive(false);
+    }
+    
+    public void CloseVolumeBoard()
+    {
+        volumeBoard.SetActive(false);
+    }
+
+    public void CloseKeybindingsBoard()
+    {
+        keybindingsBoard.SetActive(false);
     }
 }
--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Music : MonoBehaviour
 {
+    public Slider volumeSlider;
+    AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnEnable()
+    {
+        //for slider events
+        volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(volumeSlider.value); });
+    }
+
+    void ChangeVolume(float sliderValue)
+    {
+        audioSource.volume = sliderValue;
     }
 }
